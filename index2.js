7f37d9e36c1aa46b8a3a15b011e34923004167a5
Noella Dandel <noelladandel@Noellas-MacBook-Pro.local>
2020-02-17 15:13:40 +0100

    index

---
+++ index2.js
@@ -0,0 +1,4 @@
+import{stringlengte} from "./library.mjs";
+
+document.getElementById('enter').addEventListener('click',()=>{
+    console.log(stringlengte(document.getElementById('invoer').value))});
